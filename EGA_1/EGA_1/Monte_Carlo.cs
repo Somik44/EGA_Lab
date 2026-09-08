d1c57a8b9148b233392e21957112d3cb2dc85f43
Somik44 <Somik.228@yandex.ru>
2026-09-08 23:46:32 +0300

    Первая лаба по ЭГА

---
+++ EGA_1/EGA_1/Monte_Carlo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGA_1
+{
+    internal class Monte_Carlo
+    {
+        public void GetMap(Dictionary<string, int> map, int L)
+        {
+            Random random = new Random();
+            Console.WriteLine("Ландшафт приспособленности:");
+            string nowValue;
+            int i;
+            for (i = 0; i < Math.Pow(2,L); i++)
+            {
+                nowValue = Convert.ToString(i, 2).PadLeft(L, '0');
+                map[nowValue] = random.Next(1, 101);
+            }
+
+            i = 0;
+            foreach(KeyValuePair<string, int> pair in map)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+                i++;
+                if (i == 32) break;
+            }
+        }
+
+        public void Algorithm(Dictionary<string, int> map, int N)
+        {
+            string maxS="";
+            int max=0, oldMax = 0;
+            bool flag = false;
+            Random rnd = new Random();
+
+            List<string> keys = map.Keys.ToList();
+
+            for (int i =0; i<N+1; i++)
+            {
+                int index = rnd.Next(keys.Count);
+                var nowKey = keys[index];
+                int nowValue = map[nowKey];
+
+                if (i == 0)
+                {
+                    maxS = nowKey; 
+                    max = nowValue;
+                }
+                else
+                {
+                    if (max < nowValue)
+                    {
+                        oldMax = max;
+                        maxS = nowKey;
+                        max = nowValue;
+                        flag = true;
+                    }
+                }
+                if (i == 0) Console.Write($"\nSet: Value = {max} Code = {maxS}\n");
+                else Console.Write($"\n{i}) MAX: {max} - {maxS}; Now Value: {nowValue} - {nowKey};");
+                
+                if (flag) { 
+                    Console.Write($" {oldMax} -> {max}");
+                    flag = false;
+                }
+                
+            }
+
+            Console.WriteLine($"\n\nИтоговое решение: {maxS} - {max}");
+        }
+
+    }
+}
---
+++ EGA_1/EGA_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EGA_1
+{
+    class Program()
+    {
+        static void Main(string[] args)
+        {
+            Monte_Carlo carlo = new Monte_Carlo();
+            const int L = 15, N = 32;
+
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            carlo.GetMap(map, L);
+            carlo.Algorithm(map, N);
+        }
+    }
+}
