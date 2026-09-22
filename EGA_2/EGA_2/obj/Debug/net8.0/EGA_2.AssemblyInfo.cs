05b94f7de275e0ff1b6e63b0c775fc2f9d34035c
Somik44 <Somik.228@yandex.ru>
2026-09-22 13:55:25 +0300

    третья лаба

--- EGA_2/EGA_2/obj/Debug/net8.0/EGA_2.AssemblyInfo.cs
+++ EGA_2/EGA_2/obj/Debug/net8.0/EGA_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EGA_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bdb4c995b40228a851811dbfa685a8a59d5e5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7676da680043351564c4124ba735186681b1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EGA_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EGA_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EGA_3/EGA_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using EGA_2;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        HillСlimbing hill = new HillСlimbing();
+
+        const int L = 5, N = 32;
+        Dictionary<string, int> map = new Dictionary<string, int>();
+        hill.CreateMap(map, L);
+        hill.Climbing(map, N);
+    }
+}
